6059e111106683018f6509e206f623fffbf31404
natizui <izui.nm@gmail.com>
2018-06-14 18:13:50 -0300

    ok button css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-import Template1 from './components/template1/template1.js'
+import Template1 from './components/template1/Template1.js'
 
 
 class App extends Component {
