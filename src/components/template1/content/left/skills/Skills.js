cfc6e14ff26aa5a2729e60446b2364a04c2908a6
natizui <izui.nm@gmail.com>
2018-06-19 04:59:28 -0300

    create dynamic list ok

--- src/components/template1/content/left/skills/Skills.js
+++ src/components/template1/content/left/skills/Skills.js
@@ -1,35 +1,55 @@
-import React, { Component } from 'react';
-import Li from './Li.js'
-import data from '../../data.json';
+import React from 'react';
 
 import '../../../Template1.css';
 
 
-let Skills = React.createClass( {
-    createListItem: function (item) {
-        return <Li source={item} key={item} />;
-      },
-    
-      createList: function (items) {
-        return items.map(this.createListItem);
-      },
-
-
-    render() {
-        return (
-            <div class="template1__skills">
-                <div class="template1__skills__content">
-                    <h2>HABILIDADES</h2>
-                    <ul>
-                        <li></li>
-                        {this.createListItem(data.items)}
-                    </ul>
-                </div>
-            </div>
-        );
+const ListItem = ({ value, onClick }) => (
+    <li onClick={onClick}>{value}</li>
+);
+  
+const List = ({ items, onItemClick }) => (
+    <ul>
+        {
+        items.map((item, i) => <ListItem key={i} value={item} onClick={onItemClick} />)
+        }
+    </ul>
+);
+  
+class Skills extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        inputValue: '',
+        skills: ['HTML', 'CSS', 'JavaScript']
+        };
     }
-})
-
+  
+    onClick = () => {
+      const { inputValue, skills } = this.state;
+      if (inputValue) {
+        const nextState = [...skills, inputValue];
+        this.setState({ skills: nextState, inputValue: '' });
+      }
+    }
+  
+    onChange = (e) => this.setState({ inputValue: e.target.value });
+  
+    handleItemClick = (e) => {console.log(e.target.innerHTML)}
+  
+    render() {
+      const { skills, inputValue } = this.state;
+      return (
+        <div class="template1__skills">
+            <div class="template1__skills__content">
+                <h2>HABILIDADES</h2>
+                <input type="text" value={inputValue} onChange={this.onChange} />
+                <button className="btn" onClick={this.onClick}>OK</button>
+                <List items={skills} onItemClick={this.handleItemClick} />
+            </div>
+        </div>
+      );
+    }
+  }
 
 
 export default Skills;
